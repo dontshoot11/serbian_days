d78e483d5a061a2ddaaf5b49dbc8f50f8e88287f
Artem Nikitin <a.nikitin@dataduck.com>
2024-01-13 18:56:17 +0100

    update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	build: {
-		assets: "assets",
-		site: "https://dontshoot11.github.io",
-		base: "/serbian_days",
-	},
+	assets: "assets",
+	site: "https://dontshoot11.github.io",
+	base: "/serbian_days",
 });
