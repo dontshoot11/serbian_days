17a20f738e101afe3f35657aa26130e2bd6ad3a1
Artem Nikitin <a.nikitin@dataduck.com>
2024-01-13 20:45:32 +0100

    update settings

---
+++ README.md
@@ -0,0 +1,1 @@
+https://dontshoot11.github.io/serbian_days/
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-
+import relativeLinks from "astro-relative-links";
 export default defineConfig({
 	build: {
 		assets: "assets",
 	},
 	site: "https://dontshoot11.github.io",
-	base: "/serbian_days",
+	integrations: [relativeLinks()],
 });
