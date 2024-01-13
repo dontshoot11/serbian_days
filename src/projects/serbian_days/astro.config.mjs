04bc73767b2da35d13bad16f2dea8d158813d5fd
Artem Nikitin <a.nikitin@dataduck.com>
2024-01-13 17:43:20 +0100

    update package ver

--- src/projects/serbian_days/astro.config.mjs
+++ src/projects/serbian_days/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	build: {
 		assets: "assets",
-		site: "https://dontshoot11.github.io/serbian_days",
+		site: "https://dontshoot11.github.io",
 		base: "/serbian_days",
 	},
 });
