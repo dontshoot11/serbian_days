4548121c34bc1fcf0319e86633eb6e03d59fca37
Artem Nikitin <a.nikitin@dataduck.com>
2024-01-21 18:19:44 +0100

    update new page

--- src/projects/serbian_days/src/components/Header/const/const.ts
+++ src/projects/serbian_days/src/components/Header/const/const.ts
@@ -2,7 +2,11 @@ import type { TNavigationItem } from "../types/types";
 
 export const NAVIGATION: TNavigationItem[] = [
 	{
-		href: "https://telegra.ph/Kak-pomoch-postradavshim-ot-vojny-v-Ukraine-esli-ty-v-Serbii-07-16",
-		text: "nav_help_ukraine",
+		href: "/",
+		text: "title",
+	},
+	{
+		href: "/ukraine_war_help",
+		text: "help_ukraine_title",
 	},
 ];
--- src/projects/serbian_days/src/const/const.ts
+++
@@ -1,6 +0,0 @@
-export const NAV_LINKS = {
-	HOME: "/",
-	LINK1: "/link1",
-	LINK2: "/link2",
-	LINK3: "/link3",
-};
