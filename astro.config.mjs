3cd924766aff74cf8a5710bb3d3d6ccce4302489
Artjem Nikitin <a.nikitin@space307.com>
2024-11-11 01:06:11 +0100

    fix relative links to assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import relativeLinks from "astro-relative-links";
 
 export default defineConfig({
   build: {
     assets: "assets",
   },
   site: "https://dontshoot11.github.io",
+  integrations: [relativeLinks()],
 });
